5b1bc3e22e641dd5a042c330bdd2910d7cba43de
dmccoystephenson <dmccoystephenson@gmail.com>
2025-05-24 10:47:24 -0600

    Integrate location and environment services, update drawEnvironment function, and adjust grid size

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from operator import truediv
 import random
 import pygame
 from environment import Environment
+from environmentService import EnvironmentService
 from graphik import Graphik
+from locationService import ServiceConfig
+from locationService import LocationService
+from location import Location
 
 
 black = (0,0,0)
@@ -11,25 +15,41 @@ white = (255,255,255)
 displayWidth = 600
 displayHeight = 600
 
-gridSize = 50
+gridSize = 10
 
-def drawEnvironment(environment, graphik, locationWidth, locationHeight):
-    for location in environment.getGrid().getLocations():
+url = "http://localhost"
+port = 9999
+
+locationServiceConfig = ServiceConfig(viron_host=url, viron_port=port)
+locationService = LocationService(locationServiceConfig)
+
+environmentServiceBaseUrl = f"{url}:{port}"
+environmentService = EnvironmentService(environmentServiceBaseUrl)
+
+def log(message):
+    print(message)
+
+def drawEnvironment(locations, graphik, locationWidth, locationHeight):
+    for location in locations:
+        location = Location(location_id=location['locationId'], x=location['x'], y=location['y'])
         red = random.randrange(50, 200)
         green = random.randrange(50, 200)
         blue = random.randrange(50, 200)
-        graphik.drawRectangle(location.getX() * locationWidth, location.getY() * locationHeight, locationWidth, locationHeight, (red,green,blue))
+        graphik.drawRectangle(location.get_x() * locationWidth, location.get_y() * locationHeight, locationWidth, locationHeight, (red,green,blue))
 
 def main():
     pygame.init()
     gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
     graphik = Graphik(gameDisplay)
     pygame.display.set_caption("Visualizing Environment With Random Colors")
+ 
+    log("Creating environment...")
+    environment = environmentService.create_environment("Test", gridSize, gridSize)
 
-    environment = Environment("Test", gridSize)
-
-    locationWidth = displayWidth/environment.getGrid().getRows()
-    locationHeight = displayHeight/environment.getGrid().getColumns()
+    locationWidth = displayWidth/gridSize
+    locationHeight = displayHeight/gridSize
+    
+    locationsCache = {}
 
     running = True
 
@@ -38,9 +58,13 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+                
+        if locationsCache == {}:
+            log("Fetching locations from service...")
+            locationsCache = locationService.get_locations_in_environment(environment.getEnvironmentId())
             
         gameDisplay.fill(white)
-        drawEnvironment(environment, graphik, locationWidth, locationHeight)
+        drawEnvironment(locationsCache, graphik, locationWidth, locationHeight)
         pygame.display.update()
 
 main()
